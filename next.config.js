2aa9520aa6fab5a7896dbefa6a8c8f6f3506b142
Matan Shaviro <matan3sh@gmail.com>
2022-11-01 18:10:00 +0200

    Init project + implement layout preparation

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    dangerouslyAllowSVG: true,
+    domains: [
+      "tailwindui.com",
+      "images.unsplash.com",
+      "eincode.com",
+      "thrangra.sirv.com",
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
