d158be4a73fe65e5d26c96c415951f3b770a060f
Matan Shaviro <matan3sh@gmail.com>
2022-11-07 20:02:39 +0200

    Improve functions implementation in md.ts file

--- lib/md.ts
+++ lib/md.ts
@@ -3,13 +3,22 @@ import fs from "fs";
 
 const getDir = (path: string): string => join(process.cwd(), path);
 
+const BLOG_DIR = getDir("/content/blogs");
+
 const getFileNames = (dir: string): string[] => {
   return fs.readdirSync(dir);
 };
+
+const getBlogFileNames = () => getFileNames(BLOG_DIR);
 
 const getItemInPath = (filePath: string): string => {
   const fileContent = fs.readFileSync(filePath, "utf8");
   return fileContent;
 };
 
-export { getFileNames, getDir, getItemInPath };
+const getBlog = (fileName: string) => {
+  const blog = getItemInPath(join(BLOG_DIR, fileName));
+  return blog;
+};
+
+export { getBlogFileNames, getBlog };
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,7 @@ import Link from "next/link";
 import { BlogList } from "../components/blogs";
 import { PortfolioList } from "../components/portfolios";
 import { BaseLayout } from "../components/layouts";
-import { getDir, getFileNames, getItemInPath } from "../lib/md";
-import { join } from "path";
+import { getBlogFileNames, getBlog } from "../lib/md";
 
 const Home: NextPage = () => {
   return (
@@ -29,12 +28,10 @@ const Home: NextPage = () => {
 };
 
 export const getStaticProps: GetStaticProps = () => {
-  const blogDir = getDir("/content/blogs");
-  const blogFileNames = getFileNames(blogDir);
+  const blogFileNames = getBlogFileNames();
 
   blogFileNames.forEach((blogFileName) => {
-    const blogContent = getItemInPath(join(blogDir, blogFileName));
-    console.log(blogContent);
+    const blogContent = getBlog(blogFileName);
   });
 
   return {
