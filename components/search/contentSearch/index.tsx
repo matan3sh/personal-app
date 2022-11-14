ab514d7e0deb8314a97f80e452e80e5cd505db98
Matan Shaviro <matan3sh@gmail.com>
2022-11-14 22:05:05 +0200

    Improve UX in content search component

--- components/search/contentSearch/index.tsx
+++ components/search/contentSearch/index.tsx
@@ -1,15 +1,48 @@
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useEffect, useRef, useState } from "react";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
 import contentIndexer from "@lib/client/contentIndexer";
 import { SearchContent } from "@interfaces/Markdown";
 
 const ContentSearch = () => {
+  const ref = useRef<HTMLInputElement>(null);
   const [results, setResults] = useState<SearchContent[]>([]);
+  const [query, setQuery] = useState<string>("");
+
+  const handleClickOutside = () => {
+    setResults([]);
+    setQuery("");
+  };
+
+  useEffect(() => {
+    const callback = (event: MouseEvent) => {
+      if (
+        results.length > 0 &&
+        ref.current &&
+        !ref.current.contains(event.target as Node)
+      ) {
+        handleClickOutside();
+      }
+    };
+
+    const escapeKeyCallback = (event: KeyboardEvent) => {
+      if (event.key === "Escape" && results.length > 0) {
+        handleClickOutside();
+      }
+    };
+
+    document.addEventListener("click", callback);
+    document.addEventListener("keydown", escapeKeyCallback);
+    return () => {
+      document.removeEventListener("click", callback);
+      document.removeEventListener("keydown", escapeKeyCallback);
+    };
+  }, [results.length]);
 
   const performSearch = (event: ChangeEvent<HTMLInputElement>) => {
     const { value } = event.target;
     const results = contentIndexer.search(value);
     setResults(results);
+    setQuery(value);
   };
 
   return (
@@ -25,6 +58,8 @@ const ContentSearch = () => {
           />
         </div>
         <input
+          value={query}
+          ref={ref}
           id="search-input"
           onChange={performSearch}
           autoComplete="off"
