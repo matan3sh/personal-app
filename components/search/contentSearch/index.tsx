152ec8baffb705989791950f9ff9e662d75d0ba8
Matan Shaviro <matan3sh@gmail.com>
2022-11-17 19:33:38 +0200

    Add navigation to searched content

--- components/search/contentSearch/index.tsx
+++ components/search/contentSearch/index.tsx
@@ -1,10 +1,12 @@
 import { ChangeEvent, useEffect, useRef, useState } from "react";
+import { useRouter } from "next/router";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
-import contentIndexer from "@lib/client/contentIndexer";
 import { SearchContent } from "@interfaces/Markdown";
+import contentIndexer from "@lib/client/ContentIndexer";
 
 const ContentSearch = () => {
   const ref = useRef<HTMLInputElement>(null);
+  const router = useRouter();
   const [results, setResults] = useState<SearchContent[]>([]);
   const [query, setQuery] = useState<string>("");
 
@@ -76,7 +78,7 @@ const ContentSearch = () => {
           {results.map((result) => (
             <li
               key={result.slug}
-              onClick={() => {}}
+              onClick={() => router.push(`/${result.category}/${result.slug}`)}
               className={`hover:bg-indigo-600 hover:text-white p-3 relative cursor-pointer`}
             >
               <div className="font-bold text-sm truncate">{result.title}</div>
