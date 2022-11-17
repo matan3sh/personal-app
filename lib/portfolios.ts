a7c5159808fbb8a0da7cdcbf1a15276909b8689c
Matan Shaviro <matan3sh@gmail.com>
2022-11-17 18:25:44 +0200

    Implement Portfolio Detail Page and completed portfolio lib

--- lib/portfolios.ts
+++ lib/portfolios.ts
@@ -1,11 +1,23 @@
 import { join } from "path";
 import { Portfolio } from "@interfaces/Portfolio";
-import { getAllItems, getDir, getFileNames, getItemInPath } from "./md";
+import {
+  getAllItems,
+  getDir,
+  getFileNames,
+  getItemInPath,
+  markdownToHtml,
+} from "./md";
 
 const PORTFOLIO_DIR = getDir("/content/portfolios");
 
-const getPortfolioFileNames = (): string[] => {
+const getPortfoliosFileNames = () => {
   return getFileNames(PORTFOLIO_DIR);
+};
+
+const getPortfolioSlugs = () => {
+  return getPortfoliosFileNames().map((fileName) =>
+    fileName.replace(/\.md$/, "")
+  );
 };
 
 const getPortfolio = (fileName: string): Portfolio => {
@@ -15,8 +27,21 @@ const getPortfolio = (fileName: string): Portfolio => {
 };
 
 const getPortfolios = (): Portfolio[] => {
-  const names = getPortfolioFileNames();
+  const names = getPortfoliosFileNames();
   return getAllItems(names, getPortfolio) as Portfolio[];
 };
 
-export { getPortfolios };
+const getPortfolioBySlug = (slug: string) => {
+  const fileName = slug + ".md";
+  return getPortfolio(fileName);
+};
+
+const getPortfolioBySlugWithMarkdown = async (
+  slug: string
+): Promise<Portfolio> => {
+  const portfolio = getPortfolioBySlug(slug);
+  portfolio.content = await markdownToHtml(portfolio.content);
+  return portfolio;
+};
+
+export { getPortfolios, getPortfolioBySlugWithMarkdown, getPortfolioSlugs };
